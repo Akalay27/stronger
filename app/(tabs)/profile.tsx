3d08451519aacf7ac7c4e56263ead63a5e6b1479
CharlieEngler3 <charlieengler3@gmail.com>
2025-05-05 21:31:57 -0400

    Blanked out profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,9 @@
-import { StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Animated from 'react-native-reanimated';
 
 import { useThemeColor } from '@/hooks/useThemeColor';
 
-// TODO: Combine container elements into one
-import { VerticalSpacer } from '@/components/VerticalSpacer';
 import { Title } from '@/components/Title';
-
-import { GenerateButton } from '@/components/ui/GenerateButton';
-import { InfoContainer } from "@/components/ui/InfoContainer";
-import { StartButton } from '@/components/ui/StartButton';
 
 export default function ProfileScreen() {
     const insets = useSafeAreaInsets();
@@ -22,49 +15,7 @@ export default function ProfileScreen() {
             backgroundColor: backgroundColor,
             paddingTop: insets.top + 90,
         }}>
-            <Title type='h1'>Start a Workout</Title>
-
-            <VerticalSpacer gap={20}/>
-
-            <Title type='h2' lightColor='#888888'>Get Started</Title>
-
-            <VerticalSpacer gap={20}/>
-
-            <View style={[ styles.container ]}>
-                <StartButton/>
-                <GenerateButton/>
-            </View>
-
-            <VerticalSpacer gap={20}/>
-
-            <Title type='h2' lightColor='#888888'>Stored Templates</Title>
-
-            <VerticalSpacer gap={20}/>
-
-            <View style={styles.container}>
-                <InfoContainer
-                    descriptionText="Leg Extension, Squat (Barbell), Calf Raise on Leg Press"
-                    timerText="2 days ago"
-                    titleText="Legs and Core"
-                />
-
-                <InfoContainer
-                    descriptionText="Lat Pulldowns, Drag Curls, Cable Rows, Bayesian Curls, Dumbbell Curls"
-                    timerText="5 days ago"
-                    titleText="Back and Bis"
-                />
-            </View>
+            <Title type='h1'>Profile</Title>
         </Animated.ScrollView>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-
-        gap: 10,
-
-        marginLeft: 10,
-        marginRight: 10,
-    },
-});
